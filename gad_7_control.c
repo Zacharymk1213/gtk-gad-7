0b5756a47c5b4ac0c089e3e5c2d3934cd78e9d9f
Zachary Kleiman <kleimanpolitics@gmail.com>
2025-07-03 14:06:45 -0400

    scoring works

--- gad_7_control.c
+++ gad_7_control.c
@@ -1,12 +1,56 @@
 #include <gtk/gtk.h>
 #include <glib/gstdio.h>
 
-static void
-print_hello (GtkWidget   *widget,
-             gpointer     data)
-{
-    g_print ("Hello World\n");
+
+// Global variables to store radio button groups
+static GtkCheckButton *q1_buttons[4];
+static GtkCheckButton *q2_buttons[4];
+static GtkCheckButton *q3_buttons[4];
+static GtkCheckButton *q4_buttons[4];
+static GtkCheckButton *q5_buttons[4];
+static GtkCheckButton *q6_buttons[4];
+static GtkCheckButton *q7_buttons[4];
+
+static GtkLabel *score_label;
+
+// Function to calculate and display the total score
+static void calculate_score(void) {
+    int total_score = 0;
+    
+    // Check each question group
+    GtkCheckButton* question_groups[7][4] = {
+        {q1_buttons[0], q1_buttons[1], q1_buttons[2], q1_buttons[3]},
+        {q2_buttons[0], q2_buttons[1], q2_buttons[2], q2_buttons[3]},
+        {q3_buttons[0], q3_buttons[1], q3_buttons[2], q3_buttons[3]},
+        {q4_buttons[0], q4_buttons[1], q4_buttons[2], q4_buttons[3]},
+        {q5_buttons[0], q5_buttons[1], q5_buttons[2], q5_buttons[3]},
+        {q6_buttons[0], q6_buttons[1], q6_buttons[2], q6_buttons[3]},
+        {q7_buttons[0], q7_buttons[1], q7_buttons[2], q7_buttons[3]}
+    };
+    
+    for (int q = 0; q < 7; q++) {
+        for (int i = 0; i < 4; i++) {
+            if (question_groups[q][i] && gtk_check_button_get_active(question_groups[q][i])) {
+                total_score += i; // Value is 0, 1, 2, or 3
+                break;
+            }
+        }
+    }
+    
+    // Update score display
+    gchar *score_text = g_strdup_printf("Score: %d", total_score);
+    gtk_label_set_text(score_label, score_text);
+    g_free(score_text);
+    
+    g_print("GAD-7 Score: %d\n", total_score);
 }
+// Callback for radio button changes
+static void on_radio_button_toggled(GtkCheckButton *button, gpointer user_data) {
+    calculate_score();
+}
+
+
+
 
 static void
 quit_cb (GtkWindow *window)
@@ -20,11 +64,53 @@ static void activate (GtkApplication *app,
     GtkBuilder *builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "gad_7_main.ui", NULL);
 
-
     // connect signal handlers to widgets
     GObject *window = gtk_builder_get_object (builder, "window");
     gtk_window_set_application (GTK_WINDOW (window), app);
 
+    // Get radio buttons for each question
+    q1_buttons[0] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "first_question_0"));
+    q1_buttons[1] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "first_question_1"));
+    q1_buttons[2] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "first_question_2"));
+    q1_buttons[3] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "first_question_3"));
+
+    q2_buttons[0] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "second_question_0"));
+    q2_buttons[1] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "second_question_1"));
+    q2_buttons[2] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "second_question_2"));
+    q2_buttons[3] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "second_question_3"));
+
+    q3_buttons[0] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "third_question_0"));
+    q3_buttons[1] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "third_question_1"));
+    q3_buttons[2] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "third_question_2"));
+    q3_buttons[3] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "third_question_3"));
+    q4_buttons[0] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "fourth_question_0"));
+    q4_buttons[1] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "fourth_question_1"));
+    q4_buttons[2] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "fourth_question_2"));
+    q4_buttons[3] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "fourth_question_3"));
+    q5_buttons[0] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "fifth_question_0"));
+    q5_buttons[1] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "fifth_question_1"));
+    q5_buttons[2] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "fifth_question_2"));
+    q5_buttons[3] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "fifth_question_3"));
+    q6_buttons[0] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "sixth_question_0"));
+    q6_buttons[1] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "sixth_question_1"));
+    q6_buttons[2] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "sixth_question_2"));
+    q6_buttons[3] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "sixth_question_3"));
+    q7_buttons[0] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "seventh_question_0"));
+    q7_buttons[1] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "seventh_question_1"));
+    q7_buttons[2] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "seventh_question_2"));
+    q7_buttons[3] = GTK_CHECK_BUTTON(gtk_builder_get_object(builder, "seventh_question_3"));
+
+    score_label = GTK_LABEL(gtk_builder_get_object(builder, "sum_label"));
+    // Connect signals for all radio buttons
+    for (int i = 0; i < 4; i++) {
+        g_signal_connect(q1_buttons[i], "toggled", G_CALLBACK(on_radio_button_toggled), NULL);
+        g_signal_connect(q2_buttons[i], "toggled", G_CALLBACK(on_radio_button_toggled), NULL);
+        g_signal_connect(q3_buttons[i], "toggled", G_CALLBACK(on_radio_button_toggled), NULL);
+        g_signal_connect(q4_buttons[i], "toggled", G_CALLBACK(on_radio_button_toggled), NULL);
+        g_signal_connect(q5_buttons[i], "toggled", G_CALLBACK(on_radio_button_toggled), NULL);
+        g_signal_connect(q6_buttons[i], "toggled", G_CALLBACK(on_radio_button_toggled), NULL);
+        g_signal_connect(q7_buttons[i], "toggled", G_CALLBACK(on_radio_button_toggled), NULL);
+    }
 
     gtk_widget_set_visible (GTK_WIDGET(window), TRUE);
 
